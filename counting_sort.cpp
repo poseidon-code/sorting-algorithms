6aeeed7a8e25f00387b00467f3020a5ffa4abe29
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2020-04-13 17:59:41 +0530

    Update counting_sort.cpp

--- counting_sort.cpp
+++ counting_sort.cpp
@@ -5,27 +5,27 @@ using namespace std;
 class CS
 {
 	private :	int *a;
-				int size;
-                int *temp;
-                int max;
+			int size;
+                	int *temp;
+                	int max;
 
 	public :	CS(int n)
-				{
-					size = n;
-					a = new int[n];
-                    temp = new int[n];
-					input();
-				};
-		
-				~CS()
-				{
-					delete a;
-				}
+			{
+				size = n;
+				a = new int[n];
+                   		temp = new int[n];
+				input();
+			};
+	
+			~CS()
+			{
+				delete a;
+			}
 				
-				void input();
-                void findMax();
-                void countingsort();
-				void display();
+			void input();
+                	void findMax();
+                	void countingsort();
+			void display();
 };
 
 // input function
@@ -45,36 +45,36 @@ void CS :: input()
 //maximum value finder
 void CS :: findMax()
 {
-    max = a[0];
-    int i;
-    for(i=1;i<size;i++)
-        if(a[i]>max)
-            max = a[i];
-    max += 1;
-    countingsort();
+	max = a[0];
+	int i;
+	for(i=1;i<size;i++)
+        	if(a[i]>max)
+           		 max = a[i];
+	max += 1;
+	countingsort();
 }
 
 //counting sort
 void CS :: countingsort()
 {
-    int i;
-    int count[10] = {0};
-    for (i=0;i<size;i++)
-        count[a[i]]++;
-    for(i=1;i<10;i++)
-        count[i] = count[i] + count[i-1];
-    for(i=size-1;i>=0;i--)
-    {
-        /*
-        t = a[i];
-        pos =--count[t]
-        temp[pos] = a[i];
-        */
-        temp[count[a[i]] - 1] = a[i];
-        count[a[i]]--;
-    }
-    for(int i=0;i<size;i++)
-        a[i] = temp[i];
+    	int i;
+    	int count[10] = {0};
+    	for (i=0;i<size;i++)
+    	    count[a[i]]++;
+    	for(i=1;i<10;i++)
+    	    count[i] = count[i] + count[i-1];
+    	for(i=size-1;i>=0;i--)
+    	{
+      	  	/*
+        	t = a[i];
+      	 	pos =--count[t]
+        	temp[pos] = a[i];
+        	*/
+        	temp[count[a[i]] - 1] = a[i];
+        	count[a[i]]--;
+    	}
+    	for(int i=0;i<size;i++)
+        	a[i] = temp[i];
 }
 
 // display function
